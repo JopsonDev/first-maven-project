7c9f9e2681e4b366b8d74bb0f97598a418735e27
JopsonDev <jopsonmason@outlook.com>
2026-04-06 14:49:34 -0400

    first push

---
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world twice");
+    }
+}
